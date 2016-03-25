14a1acca070f9dcde225e57cc90d591eda00599e
Kamil <kamil@zaripov.net>
2016-03-25 18:16:35 +0300

    Фильтр работает, но дрожжит.

--- firmware/app/nav.cpp
+++ firmware/app/nav.cpp
@@ -8,12 +8,13 @@ static THD_FUNCTION(update, arg);
 static THD_WORKING_AREA(updateWorkingArea, 4096);
 
 #define gyroMeasError (6.0f / 180.0f) * M_PI // gyroscope measurement error in rad/s (shown as 6 deg/s)
-//#define gyroMeasDrift (0.3f / 180.0f) * M_PI// gyroscope measurement error in rad/s/s (shown as 0.3f deg/s/s)
-#define deltat (NAV_UPDATE_PERIOD_MS / 1000.0f)
+#define gyroMeasDrift (0.3f / 180.0f) * M_PI// gyroscope measurement error in rad/s/s (shown as 0.3f deg/s/s)
+
+#define c_zeta sqrt(3.0f / 4.0f) * gyroMeasDrift // compute zeta
+#define c_beta sqrt(3.0f / 4.0f) * gyroMeasError // compute beta
 
 static binary_semaphore_t dataAccess;
 vectorData rpy;
-Vector<3> zeta;
 
 //Vector<3> mag_id{16.502, -49.709, 3.162}; //magnetic field coeff
 Quaternion<> q{1,0,0,0};
@@ -115,74 +116,70 @@ Matrix<4,3> Jt_E(Quaternion<> q, Vector<3> d){
 void filterUpdate(Vector<3> gyro, Vector<3> accel, Vector<3> mag) {
     // local system variables
     const Vector<3> Ae{0.0,1.0,0.0};
-    static Vector<3> Me{0.105,-0.545,-0.109}, gyroB{0.0,0.0,0.0}, gyroDrift{0,0,0};
-    double beta;
-    beta = gyroMeasError/3.0f;
-    Quaternion<> dqFromGyro, dqFromAccelMag;//, qFromAccel, qFromMag;
-    Vector<3> gyroError;
+        static Vector<3> Me{0.105,-0.545,-0.109}, gyroB{0.0,0.0,0.0};
+        Quaternion<> qFromGyro, qFromAccelMag;//, qFromAccel, qFromMag;
+        Vector<3> gyroError;
 
-    static int count = 0;
+        float zeta, beta;
+        static int count = 0;
 
-    static systime_t oldTime = chVTGetSystemTime();
+        static systime_t oldTime = chVTGetSystemTime();
 
-    double dt = ST2MS(chVTGetSystemTime() - oldTime)/1000.0f;
-    oldTime = chVTGetSystemTime();
+        double dt = ST2MS(chVTGetSystemTime() - oldTime)/1000.0f;
+        oldTime = chVTGetSystemTime();
 
-//    zeta(0) = 0.157f/30.0f;
-//    zeta(1) = 0.157f/30.0f;
-//    zeta(2) = 0.157f/30.0f;
+        accel.normalize();
+        mag.normalize();
+        Me.normalize();
 
-    if( count < 25 ) {
-        zeta(0) = 0.157f;
-        zeta(1) = 0.157f;
-        zeta(2) = 0.157f;
-        beta = 30.0f * gyroMeasError;
-        gyroDrift = gyroDrift + gyro;
-        count++;
+        if(count<25){
+            zeta = 30.0f * c_zeta;
+            beta = 30.0f * c_beta;
+            count++;
+        }
+        else{
+            zeta = c_zeta * 0.5f;
+            beta = c_beta * 0.8f;
+        }
 
-    } else if( count == 25 ) {
-        zeta = (gyroDrift/25.0f)*sqrt(3.0f/4.0f);
-        beta = gyroMeasError*sqrt(3.0f/4.0f);
-    }
+    //    // compute the quaternion from gravity field oreentation
+    //    std::function< Vector<3>( Vector<4>) > fAccel = ([&] (Vector<4> q) -> Vector<3> {
+    //         return (Quaternion<>(q).conjugated() * Ae * Quaternion<>(q)).vector() - accel;
+    //    });
+    //    qFromAccel = jacobian(fAccel, (Vector<4>)q).transponced() * fAccel(q);
 
-//    // compute the quaternion from gravity field oreentation
-//    std::function< Vector<3>( Vector<4>) > fAccel = ([&] (Vector<4> q) -> Vector<3> {
-//         return (Quaternion<>(q).conjugated() * Ae * Quaternion<>(q)).vector() - accel;
-//    });
-//    qFromAccel = jacobian(fAccel, (Vector<4>)q).transponced() * fAccel(q);
+    //    // compute the quaternion from gravity field oreentation
+    //    std::function< Vector<3>( Vector<4>) > fMag = ([&] (Vector<4> q) -> Vector<3> {
+    //         return (Quaternion<>(q).conjugated() * Me * Quaternion<>(q)).vector() - accel;
+    //    });
+    //    qFromMag = jacobian(fMag, (Vector<4>)q).transponced() * fMag(q);
 
-//    // compute the quaternion from gravity field oreentation
-//    std::function< Vector<3>( Vector<4>) > fMag = ([&] (Vector<4> q) -> Vector<3> {
-//         return (Quaternion<>(q).conjugated() * Me * Quaternion<>(q)).vector() - accel;
-//    });
-//    qFromMag = jacobian(fMag, (Vector<4>)q).transponced() * fMag(q);
+    //    qFromAccelMag = qFromAccel + qFromMag;
 
-//    qFromAccelMag = qFromAccel + qFromMag;
+        // compute the quaternion from gravity field oreentation
+        std::function< Vector<6>( Vector<4>) > f = ([&] (Vector<4> q) -> Vector<6> {
+            Vector<3> fromAccel = (Quaternion<>(q).conjugated() * Ae * Quaternion<>(q)).vector() - accel;
+            Vector<3> fromMag = (Quaternion<>(q).conjugated() * Me * Quaternion<>(q)).vector() - mag;
+            return Vector<6>{ fromAccel(0), fromAccel(1), fromAccel(2), fromMag(0), fromMag(1), fromMag(2) };
+        });
+        qFromAccelMag = jacobian(f, (Vector<4>)q).transponced() * f(q);
+        qFromAccelMag.normalize();
 
-    // compute the quaternion from gravity field oreentation
-    std::function< Vector<6>( Vector<4>) > f = ([&] (Vector<4> q) -> Vector<6> {
-        Vector<3> fromAccel = (Quaternion<>(q).conjugated() * Ae * Quaternion<>(q)).vector() - accel;
-        Vector<3> fromMag = (Quaternion<>(q).conjugated() * Me * Quaternion<>(q)).vector() - mag;
-        return Vector<6>{ fromAccel(0), fromAccel(1), fromAccel(2), fromMag(0), fromMag(1), fromMag(2) };
-    });
-    dqFromAccelMag = jacobian(f, (Vector<4>)q).transponced() * f(q);
-    dqFromAccelMag.normalize();
+        // compute angular estimated direction of the gyroscope error
+        gyroError = (q.conjugated() * qFromAccelMag).vector()*2.0f;
+        // compute and remove the gyroscope baises
+        gyroB = gyroB + gyroError * dt * zeta;
+        gyro = gyro - gyroB;
 
-    // compute angular estimated direction of the gyroscope error
-    gyroError = (q.conjugated() * dqFromAccelMag).vector()*2.0f;
-    // compute and remove the gyroscope baises
-    gyroB = gyroB + gyroError * zeta;
-    gyro = gyro - gyroB;
+        // compute the quaternion rate measured by gyroscopes
+        qFromGyro = (q * Quaternion<>(0,gyro))*0.5f;
 
-    // compute the quaternion rate measured by gyroscopes
-    dqFromGyro = (q * Quaternion<>(0,gyro))*0.5f;
-
-    // compute then integrate the estimated quaternion rate
-    q = q + (dqFromGyro - (beta * dqFromAccelMag)) * dt;
-    // normalise quaternion
-    q.normalize();
-    // compute flux in the earth frame
-    Me = (q * mag * q.conjugated()).vector();
+        // compute then integrate the estimated quaternion rate
+        q = q + (qFromGyro - (beta * qFromAccelMag)) * dt;
+        // normalise quaternion
+        q.normalize();
+        // compute flux in the earth frame
+        Me = (q * mag * q.conjugated()).vector();
 
 }
 
@@ -192,13 +189,12 @@ THD_FUNCTION(update, arg) {
     Quaternion<> dq;
     Vector<3> gyroError;
 
-    int count = 0;
     while( !chThdShouldTerminateX() ) {
 //        chibios_time += MS2ST(NAV_UPDATE_PERIOD_MS);
 
         static systime_t oldTime = chVTGetSystemTime();
 
-        double dt = ST2MS(chVTGetSystemTime() - oldTime)/1000.0f;
+        float dt = ST2MS(chVTGetSystemTime() - oldTime)/1000.0f;
         oldTime = chVTGetSystemTime();
 
         gyro = {imuGyroGet().x, imuGyroGet().y, imuGyroGet().z};
@@ -206,28 +202,9 @@ THD_FUNCTION(update, arg) {
         accel = {imuAccelGet().x, imuAccelGet().y, imuAccelGet().z};
         mag = {imuMagGet().x, imuMagGet().y, imuMagGet().z};
 
-//        if( count < 25 ) {
-//            gyroDrift = gyroDrift + gyro;
-//            count++;
-
-              accel.normalize();
-              mag.normalize();
-
-//            std::function< Vector<6>( Vector<4>) > f = ([&] (Vector<4> q) -> Vector<6> {
-//                Vector<3> fromAccel = (Quaternion<>(q).conjugated() * Ae * Quaternion<>(q)).vector() - accel;
-//                Vector<3> fromMag = (Quaternion<>(q).conjugated() * Me * Quaternion<>(q)).vector() - mag;
-//                return Vector<6>{ fromAccel(0), fromAccel(1), fromAccel(2), fromMag(0), fromMag(1), fromMag(2) };
-//            });
-//            dq = jacobian(f, (Vector<4>)q).transponced() * f(q);
-//            dq.normalize();
-//            q = q + (((6.0f / 180.0f) * M_PI)/3.0f * dq) * dt;
-//        } else if( count == 25 ) {
-//            zeta = gyroDrift/25.0f;
-//        } else {
-            chBSemWait(&dataAccess);
-            filterUpdate(gyro, accel, mag);
-            chBSemSignal(&dataAccess);
-    //    }
+        chBSemWait(&dataAccess);
+        filterUpdate(gyro, accel, mag);
+        chBSemSignal(&dataAccess);
 
 
         //углы Эйлера в радианах
